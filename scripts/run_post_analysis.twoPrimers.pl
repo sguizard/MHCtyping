d0aebe9e3d46f2cc3b1745809c316a234ee23aca
Sébastien Guizard <sguizard@edu.ac.uk>
2025-05-27 10:22:37 +0100

    Reformat, update input/output paths, add missing database option

--- scripts/run_post_analysis.twoPrimers.pl
+++ scripts/run_post_analysis.twoPrimers.pl
@@ -4,20 +4,14 @@ use Cwd;
 my $samplesheet = "samplesheet.txt";
 my $work_dir    = "results";
 my $prefix      = "run";
-# my $primer1 = "For1Rev2";
-# my $primer2 = "For3Rev1";
-
-my $cutoff = 0.2;
-my $fc = 3;
+my $cutoff      = 0.2;
+my $fc          = 3;
 
 GetOptions(
     'samplesheet=s' => \$samplesheet,
     'work_dir=s'    => \$work_dir,
+    'database=s'  => \$database,
     'prefix=s'      => \$prefix
-    # 'primer1=s'     => \$primer1,
-    # 'primer2=s'	  => \$primer2,
-    # 'cutoff=i'	  => \$cutoff,
-    # 'fc=i'	      => \$fc
 ) or print "Invalid options\n";
 
 
@@ -30,23 +24,33 @@ while(<SAMPLESHEET>){
 	print "==> Running $sample...\n";
     print "===> CMD: perl scripts/overlap_mhcI.pl \\
         --sample=$sample \\
-        --primer1=results/$sample/$sample.For1Rev2.selected.fasta \\
-        --primer2=results/$sample/$sample.For3Rev1.selected.fasta\n\n";
-	system ("perl scripts/overlap_mhcI.pl --sample=$sample --primer1=results/$sample/$sample.For1Rev2.selected.fasta --primer2=results/$sample/$sample.For1Rev2.selected.fasta");
+        --primer1=results/$sample/06-filtering/$sample.For1Rev2.selected.fasta \\
+        --primer2=results/$sample/06-filtering/$sample.For3Rev1.selected.fasta\n\n";
+	system ("
+        perl scripts/overlap_mhcI.pl \\
+            --sample=$sample \\
+            --primer1=results/$sample/06-filtering/$sample.For1Rev2.selected.fasta \\
+            --primer2=results/$sample/06-filtering/$sample.For1Rev2.selected.fasta");
     print "===> DONE $sample\n\n";
 }
 print "=> Apply overlap_mhcI.pl on samples ... Done \n\n\n";
 
 
-print "=> Running makeSummaryOverlappedPrimers.pl";
-print "==> CMD: perl scripts/makeSummaryOverlappedPrimers.pl \\
+print "=> Running makeSummaryOverlappedPrimers.pl\n";
+print "==> Running $sample...\n";
+print "===> CMD: perl scripts/makeSummaryOverlappedPrimers.pl \\
     --samplesheet=$samplesheet \\
     --prefix=$prefix \\
     --cutoff=$cutoff \\
-    --database=fasta/MHCI.fasta \\
+    --database=$database \\
     --fc=$fc\n\n";
-system ("perl scripts/makeSummaryOverlappedPrimers.pl --samplesheet=$samplesheet --prefix=$prefix --cutoff=$cutoff --database=fasta/MHCI.fasta --fc=$fc");
-print "==> DONE\n\n";
+system ("perl scripts/makeSummaryOverlappedPrimers.pl \\
+    --samplesheet=$samplesheet \\
+    --prefix=$prefix \\
+    --cutoff=$cutoff \\
+    --database=$database \\
+    --fc=$fc");
+print "===> DONE\n\n";
 
 
 system ("cat summary/*.mhcI.selected.tsv > $prefix.mhcI.selected.tsv");
